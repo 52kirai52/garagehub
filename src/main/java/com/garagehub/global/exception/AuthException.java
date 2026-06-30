265a1ab765659a437f6de7e39370f8eeb86ae237
52kirai52 <52kirai52@gmail.com>
2026-06-30 10:35:08 +0900

    refactor: 커스텀 예외를 하나로 통일 (ErrorCode에서 예외의 상세 정보를 전부 관리하도록 방식을 변경)

--- src/main/java/com/garagehub/global/exception/AuthException.java
+++
@@ -1,8 +0,0 @@
-package com.garagehub.global.exception;
-
-public class AuthException extends RuntimeException {
-    public AuthException(String message){
-        super(message);
-    }
-    
-}
--- src/main/java/com/garagehub/global/exception/SmsException.java
+++
@@ -1,7 +0,0 @@
-package com.garagehub.global.exception;
-
-public class SmsException extends RuntimeException {
-    public SmsException(String message) {
-        super(message);
-    }
-}
