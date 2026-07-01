e41855d0a492093516ad2edddf880647c490c0d3
52kirai52 <52kirai52@gmail.com>
2026-07-01 12:49:33 +0900

    refactor: ErrorStatus 제거 후 HttpStatus 직접 사용으로 변경

--- src/main/java/com/garagehub/global/exception/ErrorCode.java
+++ src/main/java/com/garagehub/global/exception/ErrorCode.java
@@ -9,19 +9,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    TEST(ErrorStatus.CLIENT_ERROR, "phone", "X000", "테스트 에러입니다.");
+    TEST(HttpStatus.BAD_REQUEST, "phone", "X000", "테스트 에러입니다.");
 
-    private final ErrorStatus status;
+    private final HttpStatus status;
     private final String field;
     private final String code;
     private final String message;
 
-    @Getter
-    @RequiredArgsConstructor
-    public enum ErrorStatus {
-        CLIENT_ERROR(HttpStatus.BAD_REQUEST),
-        SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR);
-
-        private final HttpStatus httpStatus;
-    }
 }
