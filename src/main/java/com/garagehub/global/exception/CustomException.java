a6ad28ee40b90ebbb2f0cfe1a671cc8eca2a8cf0
52kirai52 <52kirai52@gmail.com>
2026-07-01 12:51:31 +0900

    feat: GlobalExceptionHandler에서 바로 사용할 수 있도록 위임 메서드 추가

--- src/main/java/com/garagehub/global/exception/CustomException.java
+++ src/main/java/com/garagehub/global/exception/CustomException.java
@@ -1,11 +1,38 @@
 package com.garagehub.global.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
-    
     private final ErrorCode errorCode;
+    private final Object data;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.data = null;
+    }
+
+    public CustomException(ErrorCode errorCode, Object data) {
+        this.errorCode = errorCode;
+        this.data = data;
+    }
+
+    public HttpStatus getStatus() {
+        return errorCode.getStatus();
+    }
+
+    public String getCode() {
+        return errorCode.getCode();
+    }
+
+    public String getField() {
+        return errorCode.getField();
+    }
+
+    @Override
+    public String getMessage() {
+        return errorCode.getMessage();
+    }
 }
